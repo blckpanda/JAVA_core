ec0333d9b70f76d85876a394231b74c848b5db1d
saurabh <saurabhsatsangi1998@gmai.com>
2024-10-24 14:41:37 +0530

    stage commit

---
+++ localInner1.java
@@ -0,0 +1,21 @@
+
+class Outer {
+    int x = 10;
+    static int y = 20;
+
+    static class My {
+        public void show() {
+            System.out.println(y);
+        }
+    }
+
+}
+
+public class Localinner1 {
+    public static void main(String[] args) {
+        Outer.My m = new Outer.My();
+        m.show();
+
+    }
+
+}
